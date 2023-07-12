812815e8bfd15d7bc83282fb915079a51e50a0ac
Zoltan Biber <zoltan.biber@gmail.com>
2023-07-12 21:37:58 +0200

    Add page and components for users with dummy data
    - Decent CSS missing for now
    - No need for placeholder .gitkeep files anymore

--- frontend/src/user/pages/Users.js
+++ frontend/src/user/pages/Users.js
@@ -1,7 +1,19 @@
 import React from "react";
 
+import UsersList from "../components/UsersList";
+
 const Users = () => {
-    return <h2>Users!!!!!!!!!!!!!!!</h2>
+  const USERS = [
+    {
+      id: "u1",
+      name: "Zoki",
+      image:
+        "https://fastly.picsum.photos/id/670/600/600.jpg?hmac=dQWeZQKRwwTp_nOqIsn2CyPpq3QQw37Xqj-JDXZ0Kvo",
+      places: 3,
+    },
+  ];
+
+  return <UsersList items={USERS} />;
 };
 
 export default Users;
